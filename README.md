9805d1aad74521be273433a459bff709ac148be7
CPdA-Omi <charlie.perrondarc@gmail.com>
2024-03-12 16:45:35 +0100

    all: version 1.5.1 (return messages translations)

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # create.sh
-## It's a shell script that is creating files for programming in linux system
+
+## It's a shell script which creates files for programming in linux system
+
 * Compatible at least with bash and zsh
 * All of the script works with arguments
 
----
 If you want help, you can enter
 ```bash
 ./create.sh -h
 ```
+
+***Finally, all of the system messages are in french as the same as the guide**
+
 ---
-## Unfortinately, all of the system messages are in french as the same as the guide
+### Last features
+
+* -html option that create html files using a template
+* all of the messages and the guide (help menu) are translated from french to english
+
+### Upcoming features
+
+* options that makes c++ files
+* optimisation of the script using more variables and rework some of them
+---
+
--- create.sh
+++ create.sh
@@ -2,45 +2,42 @@
 
 # Author : CPdA-Omi
 # Creation Date : december 6 2021
-# Last update : 03/04/2024 (MM/DD/YYYY)
+# Last update : 03/12/2024 (MM/DD/YYYY)
 
 author="CPdA-Omi"
-genMsg="Fichier généré grâce à create.sh ($author)"
+genMsg="File created by create.sh ($author)"
+
+ifCreationError() {
+	if [ $? -ne 0 ]; then
+		echo "Unknown creation error"
+		exit 1
+	fi
+}
 
 fichiersC() {
 	if [ "$mkf" = "1" ]; then
 		if [ "$tree" = "1" ]; then
-			echo '#Fichier makefile créé par Mme Carine Simon avec un "C"
-#'"$genMsg"'
+			echo '#'"$genMsg"'
 
-#CC : le compilateur à utiliser
 CC=gcc
 
-#CFLAGS : les options de compilation
 CFLAGS= -Wall -g
 
-# les fichiers sources : tous les fichiers présents dans src/
 SRC=$(wildcard src/*.c)
 
-# les fichiers objets (.o)
 OBJ=$(patsubst src/%.c,obj/%.o,$(SRC))
 
-
-#édition des liens : génération de l'\''exécutable à partir des .o 
 bin/exe: $(OBJ)
 	$(CC) $(OBJ) -o $@
 
-# génération des .o à partir des .c et .h crrespondants : 
 obj/%.o: src/%.c 
 	$(CC) $(CFLAGS) -c $< -o $@
 
-#nettoyage : destruction des .o et de l'\''exécutable
 clean:
 	rm obj/*.o bin/exe' > $way\/makefile
 		
 		else
-			echo "#Fichier makefile créé par M. Hasbani
-#$genMsg
+			echo "#$genMsg
 
 exe:	$fic.o test$fic.o
 		gcc $fic.o test$fic.o -o exe
@@ -55,30 +52,26 @@ clean:
 		rm -Rf *.o exe" > $way\/makefile
 		fi
 
-		if [ $? -ne 0 ]; then
-			exit 1
-		fi
-		echo "Création du fichier makefile"
+		ifCreationError
+		echo "Creation of the makefile"
 	fi
 
-#===============================.makefile ^==========tree v==========
+#===============================.makefile ^========-t v==============
 
 	if [ "$tree" = "1" ]; then
 		$(mkdir -p $way\/{src,obj,bin})
 
-		if [ $? -ne 0 ]; then
-			exit 1
-		fi
+		ifCreationError
 		way="$way/src"
 
 		if [ "$file" == "1" ]; then
-			echo "Création des l'arborescence dans $(pwd)/$fic/"
+			echo "Creation of the tree $(pwd)/$fic/"
 		else
-			echo "Création des l'arborescence dans $(pwd)/"
+			echo "Creation of the tree $(pwd)/"
 		fi
 	fi
 
-#====================================tree ^=========.h v=============
+#======================================-t ^========.h v==============
 
 	echo "// $genMsg
 #ifndef $(echo "$fic.h" | tr a-z A-Z | tr . ' ' | tr ' ' _)
@@ -91,10 +84,10 @@ clean:
 		exit 1
 	fi
 
-#======================================.h ^=========.c v=============
+#======================================.h ^========.c v==============
 	echo "// $genMsg
-// #include <stdio.h> // Seulement si communication avec le terminal
-// #include <stdlib.h> // Seulement si utilisation des fonctions suivantes : exit, malloc, free, system ou rand
+// #include <stdio.h> // Only for the terminal communication
+// #include <stdlib.h> // Only for uses of the following functions: exit, malloc, free, system or rand
 #include \"$fic.h\"
 
 int fct1 (void){
@@ -104,15 +97,16 @@ int fct1 (void){
 		exit 1
 	fi
 
-#======================================.c ^=========test.c v=============
+#======================================.c ^========test.c v==========
+
 	echo "// $genMsg
-#include <stdio.h> // Seulement si communication avec le terminal
+#include <stdio.h> // Only for the terminal communication
 #include \"$fic.h\"
 
 void testFct1(void){
 	int var;
 	var=fct1();
-	printf(\"Nombre de retour : %d\n\", var);
+	printf(\"Returned number: %d\n\", var);
 }
 
 int main (void){
@@ -123,9 +117,9 @@ int main (void){
 		exit 1
 	fi
 
-	echo "Création des fichiers\"$fic.h\", \"$fic.c\" et \"test$fic.c\""
+	echo "Creation of the files \"$fic.h\", \"$fic.c\" et \"test$fic.c\""
 
-#==================================================chmod v===============
+#==================================test.c ^========chmod v===========
 
 	chmod $chmod $way\/*
 
@@ -139,13 +133,15 @@ int main (void){
 		chmod a+x "$way/../makefile"
 	fi
 
+#===================================chmod ^========-o v==============
+
 	if [ "$open" = "1" ]; then
 		if [ -z $openCmd ]; then
 			subl $way\/$fic.h $way\/$fic.c $way\/test$fic.c
-			echo "Ouverture des fichiers \"$fic.h\", \"$fic.c\" et \"test$fic.c\" sous Sublime Text"
+			echo "Opening files \"$fic.h\", \"$fic.c\" et \"test$fic.c\" on Sublime Text"
 		else
 			$openCmd $way\/$fic.h $way\/$fic.c $way\/test$fic.c
-			echo "Ouverture des fichiers \"$fic.h\", \"$fic.c\" et \"test$fic.c\""
+			echo "Opening files \"$fic.h\", \"$fic.c\" et \"test$fic.c\""
 		fi
 	fi
 	exit 0
@@ -157,14 +153,14 @@ creation() {
 	if [ "$file" = "1" ]; then
 		mkdir $fic
 		if [ $? -eq 0 ]; then
-			echo "Répertoire \"$fic\" créé"
+			echo "Directory \"$fic\" created"
 		else
 			exit 1
 		fi
 		way="$fic"
 	fi
 
-#======================================-d ^=========-c v=============
+#======================================-d ^========-c v==============
 
 	if [ "$c" = "1" ]; then
 		fichiersC
@@ -177,11 +173,9 @@ creation() {
 #$genMsg
 
 echo 'Hello, World!'" > $way\/$fic
-		if [ $? -ne 0 ]; then
-			exit 1
-		fi
+		ifCreationError
 
-#======================================-sh ^=======-py v=============
+#=====================================-sh ^========-py v=============
 
 	elif [ "$py" = "1" ]; then
 		fic="$fic.py"
@@ -190,11 +184,9 @@ echo 'Hello, World!'" > $way\/$fic
 if __name__ == '__main__':
 
 	print(\"Hello, World!\")" > $way\/$fic
-		if [ $? -ne 0 ]; then
-			exit 1
-		fi
+		ifCreationError
 
-#======================================-py ^=======-html v===========
+#=====================================-py ^========-html v===========
 
 	elif [ "$html" = "1" ]; then
 		fic="$fic.html"
@@ -215,30 +207,26 @@ if __name__ == '__main__':
 		</footer>
 	</body>
 </html>" > $way\/$fic
-		if [ $? -ne 0 ]; then
-			exit 1
-		fi
+		ifCreationError
 
-#======================================-html ^=======================
+#====================================-html ^=========================
 
 	else
-		echo -e "// $genMsg\n// Aucune extension de fichier valide saisie.\n" > $way\/$fic
-		if [ $? -ne 0 ]; then
-			exit 1
-		fi
+		echo -e "// $genMsg\n// No valid file extension entered.\n" > $way\/$fic
+		ifCreationError
 	fi
 	chmod $chmod $way\/$fic
-	echo "Création du fichier \"$fic\""
+	echo "Creation of the file \"$fic\""
 
-#===================================================-o v=============
+#==================================================-o v==============
 
 	if [ "$open" = "1" ]; then
 		if [ -z $openCmd ]; then
 			subl $way\/$fic
-			echo "Ouverture du fichier \"$fic\" sous Sublime Text"
+			echo "Opening of the file \"$fic\" on Sublime Text"
 		else
 			$openCmd $way\/$fic
-			echo "Ouverture du fichier \"$fic\""
+			echo "Opening of the file \"$fic\""
 		fi
 	fi
 }
@@ -247,80 +235,78 @@ guide() {
 	echo -e "  \e[1m#\e[33m======================================================\e[0m\e[1m GUIDE $(basename $0 | tr a-z A-Z) \e[33m======================================================\e[0m\e[1m#\e[0m
   \e[1m\e[33m|																|
   |																|
-  |\e[0m	  Le fichier \"$(basename $0)\" est un programme permettant de créer rapidement des fichiers avec ou sans répertoire		\e[1m\e[33m|
-  |\e[0m	  avec des permissions pré-réglés ou non.										\e[1m\e[33m|
+  |\e[0m	  The file \"$(basename $0)\" is a program which fastly create with a lot of options described right down			\e[1m\e[33m|
   |																|
   |																|
-  |\e[0m	  \e[4m\e[33mFonctionnement :\e[0m													\e[1m\e[33m|
+  |\e[0m	  \e[4m\e[33mWorking :\e[0m														\e[1m\e[33m|
   |																|
-  |\e[0m	  Tout fonctionne par arguments pour avoir l'exécution la plus rapide possible mais le premier peut avoir		\e[1m\e[33m|
-  |\e[0m	  plusieurs utilitées :													\e[1m\e[33m|
+  |\e[0m	  All is working with arguments in order to have the fastest execusion but the first can have multiple utilities :	\e[1m\e[33m|
   |																|
-  |\e[0m	  Des droits peuvent être saisis directement en \e[36mpremier argument\e[0m (ex: 642). Ce champ est \e[36mfacultatif\e[0m.			\e[1m\e[33m|
-  |\e[0m	  Si des droits ne sont pas saisis, les droits \"755\" seront ceux par défaut.						\e[1m\e[33m|
+  |\e[0m	  Rights can be entered directly as the \e[4mfirst argument\e[0m (ex: 642). This field is \e[1moptional\e[0m.				\e[1m\e[33m|
+  |\e[0m	  If rights are not entered, the rights \"755\" will be the default ones.							\e[1m\e[33m|
   |																|
-  |\e[0m	  Droits saisis ou pas, la donnée saisit ensuite sera le nom du fichier qui sera créé par la suite et 			\e[1m\e[33m|
-  |\e[0m	  est \e[36mnécessaire\e[0m pour le bon fonctionnement du programme.								\e[1m\e[33m|
+  |\e[0m	  Then, the following value will be the file name that will be created. \e[1mThis argument is \e[4mnecessary\e[0m\e[1m for the good 	\e[1m\e[33m|
+  |\e[0m	  behaviour of the script.												\e[1m\e[33m|
   |																|
-  |\e[0m	  Tous les arguments suivants peuvent ensuite être utilisés dans n'importe quel ordre (sauf pour \"-mkf\") et		\e[1m\e[33m|
-  |\e[0m	  ont différentes utilitées décrites ci-après.										\e[1m\e[33m|
+  |\e[0m	  All the following arguments can be used in any order (except \"-mkf\" and \"-t\") and those are their functions.		\e[1m\e[33m|
   |																|
   |																|
   |\e[0m	  \e[4m\e[33mOptions :\e[0m														\e[1m\e[33m|
   |																|
-  |\e[0m	  Par défaut un fichier n'a pas de format mais trois sont pour le moment pris en charges :				\e[1m\e[33m|
+  |\e[0m	  By default, a file does not have a format but three are set in this script :						\e[1m\e[33m|
   |																|
-  |\e[0m		  \e[94m-html\e[0m  : créer le fichier en format html (avec toutes les balises conseillées) ;				\e[1m\e[33m|
-  |\e[0m		  \e[93m-py\e[0m    : créer le fichier en format python (avec un main python) ;						\e[1m\e[33m|
-  |\e[0m		  \e[96m-sh\e[0m    : créer le fichier en format shell (avec les commandes d'exécution en début de fichier) ;		\e[1m\e[33m|
-  |\e[0m		  \e[31m-c\e[0m     : créer tous les fichiers nécessaires au fonctionnement d'un programme c :				\e[1m\e[33m|
-  |\e[0m				  un fichier .c, un fichier .h et un fichier test.c avec tout ce qui est nécessaire		\e[1m\e[33m|
-  |\e[0m				  à une bonne utilisation déjà prérentré dans ces fichiers.					\e[1m\e[33m|
+  |\e[0m		  \e[94m-html\e[0m  : create the file in the html format (with all the main tags) ;					\e[1m\e[33m|
+  |\e[0m		  \e[93m-py\e[0m    : create the file in the python format (with a python main) ;						\e[1m\e[33m|
+  |\e[0m		  \e[96m-sh\e[0m    : create the file in the bash format (with the shebang) ;						\e[1m\e[33m|
+  |\e[0m		  \e[31m-c\e[0m     : create alle the files necessary to run a c program :							\e[1m\e[33m|
+  |\e[0m				  a .c file, a .h file and a test.c file with all necessary					\e[1m\e[33m|
+  |\e[0m				  imports for a good use already set in the files.						\e[1m\e[33m|
   |																|
-  |\e[0m	  \e[31m-mkf\e[0m : option créant un fichier makefile réservé aux fichiers .c et .h créés.						\e[1m\e[33m|
-  |\e[0m			  L'option \"-mkf\" ne peut être saisi \e[36mseulement après les options \"-c\" et \"-t\"\e[0m et le fichier		\e[1m\e[33m|
-  |\e[0m			  est déjà prêt à l'emploi.										\e[1m\e[33m|
+  |\e[0m	  \e[31m-mkf (makefile)\e[0m : sub-option of \"-c\" creating a makefile file for to compile using \e[3mmake\e[0m				\e[1m\e[33m|
+  |\e[0m			This sub-option \e[1mcan only be entered after \"-c\" or \"-t\"\e[0m.							\e[1m\e[33m|
   |																|
-  |\e[0m	  \e[31m-t\e[0m   : option créant toute l'arborescence de répertoire (bin, obj & src où seront les fichiers C)			\e[1m\e[33m|
-  |\e[0m			  L'option \"-t\" (pour tree) ne peut être saisi \e[36mseulement après les options \"-c\" et \"-mkf\"\e[0m car		\e[1m\e[33m|
-  |\e[0m			  relatif aux fichiers C.										\e[1m\e[33m|
+  |\e[0m	  \e[31m-t (tree)\e[0m	  : sub-option of \"-c\" creating an entire tree structure within the files :				\e[1m\e[33m|
+  |\e[0m			  - bin,												\e[1m\e[33m|
+  |\e[0m			  - obj (for the .o files created using \e[3mmake\e[0m),								\e[1m\e[33m|
+  |\e[0m			  - src (where the C files will be stored).								\e[1m\e[33m|
+  |\e[0m			This sub-option \e[1mcan only be entered after \"-c\" or \"-mkf\"\e[0m.						\e[1m\e[33m|
+  |\e[0m			\e[4mI highly recommand you to use this sub-option with the \"-o\" option.\e[0m					\e[1m\e[33m|
   |																|
-  |\e[0m	  \e[32m-d\e[0m   : option créant un répertoire dans lequel sera créé tous les fichiers ainsi créés.				\e[1m\e[33m|
-  |\e[0m	  	 Si l'option n'est pas saisit, les fichiers sont créés dans le répertoire courant.				\e[1m\e[33m|
-  |\e[0m			  Exemple : \e[36m\"./$(basename $0) toto -c -d\"\e[0m créera les fichiers \"toto.h\", \"toto.c\" et \"testtoto.c\"		\e[1m\e[33m|
-  |\e[0m			  \e[36mdans le répertoire\e[0m \"toto\".										\e[1m\e[33m|
+  |\e[0m	  \e[32m-d (directory)\e[0m  : option creating a directory where the files will be created.					\e[1m\e[33m|
+  |\e[0m	  		 If this option is not entered, all the files will be created in the actual directory (pwd).		\e[1m\e[33m|
+  |\e[0m			  Example : \e[3m./$(basename $0) bubble -c -d\e[0m will create \"bubble.h\", \"bubble.c\" and \"testbubble.c\"		\e[1m\e[33m|
+  |\e[0m			  in the \e[3mbubble\e[0m directory.										\e[1m\e[33m|
   |																|
-  |\e[0m	  \e[32m-o\e[0m   : option permettant d'ouvrir tous les fichiers créés (sauf le fichier makefile si créé).				\e[1m\e[33m|
-  |\e[0m		 Par défaut, ces fichiers sont ouverts via \e[36m\"Sublime Text\"\e[0m mais vous pourrez par exemple changer cela		\e[1m\e[33m|
-  |\e[0m		 directement en entrant à l'argument d'après la commande de votre éditeur de texte favoris.			\e[1m\e[33m|
-  |\e[0m			  Exemple : \e[36m\"./$(basename $0) lapin -sh -o gedit\"\e[0m ouvrira le fichier \"lapin.sh sur \e[36m\"éditeur de texte\"\e[0m	\e[1m\e[33m|
+  |\e[0m	  \e[32m-o (open)\e[0m   : option that opens all the created files (except the makefile if created).				\e[1m\e[33m|
+  |\e[0m		 	By default, those files are opened via \e[3mSublime Text\e[0m but you can change that by enter a command		\e[1m\e[33m|
+  |\e[0m		 	that opens a text editor right after this option (or change the lines 140 and 225 of the script).	\e[1m\e[33m|
+  |\e[0m			  Example : \e[3m./$(basename $0) rabbit -sh -o vi\e[0m will open the \"rabbit.sh\" using \e[3mvi\e[0m.				\e[1m\e[33m|
   |																|
-  |\e[0m	  \e[32m-h\e[0m   : option permettant d'ouvrir ce guide si utilisé en premier argument lors de l'appel du script.			\e[1m\e[33m|
-  |\e[0m		 En souvenir des premières version de ce script, l'option \e[36m\"-guide\"\e[0m marche encore.				\e[1m\e[33m|
-  |\e[0m			  À savoir aussi que si le guide est ouvert, aucune autre action que ferait le script			\e[1m\e[33m|
-  |\e[0m			  dans son fonctionnement normal n'a lieu.								\e[1m\e[33m|
+  |\e[0m	  \e[32m-h (help)\e[0m   : option that opens that guide if used as first argument (--help also works).				\e[1m\e[33m|
+  |\e[0m		 	In memory of the previous script versions, the \"-guide\" option still works.				\e[1m\e[33m|
+  |\e[0m			If this guide is open, no other action will be effective.						\e[1m\e[33m|
   |																|
-  |\e[0m		Une option supplémentaire prenant en charge les fichiers nécessaires aux programmes C++ est à venir...		\e[1m\e[33m|
+  |\e[0m	  Another option that create C++ files might comes later...								\e[1m\e[33m|
   |																|
   |																|
   |\e[0m	  \e[4m\e[33mExemples :\e[0m														\e[1m\e[33m|
   |																|
   |\e[0m	  ./$(basename $0) chat -d -html												\e[1m\e[33m|
-  |\e[0m	  ./$(basename $0) titi -c -mkf -d												\e[1m\e[33m|
-  |\e[0m	  ./$(basename $0) 744 tata -o -py												\e[1m\e[33m|
+  |\e[0m	  ./$(basename $0) toto -c -mkf -d												\e[1m\e[33m|
+  |\e[0m	  ./$(basename $0) 744 titi -o -py												\e[1m\e[33m|
   |\e[0m	  ./$(basename $0) lapin -o gedit -d -sh											\e[1m\e[33m|
   |\e[0m	  ./$(basename $0) 700 nounours -d -c -t -mkf										\e[1m\e[33m|
   |																|
   |																|
-  |                                                create.sh V1.5.0 (2021->2024)                                                |
-  |							Créé par CPdA-Omi							\e[1m\e[33m|
+  |						  create.sh V1.5.1 (2021->2024)  						|
+  |							Created by CPdA-Omi							\e[1m\e[33m|
   \e[0m\e[1m#\e[33m=============================================================================================================================\e[0m\e[1m#\e[0m"
 }
 
 #================================================================================Main==================================================================================#
 
 if [ $# -eq 0 ]; then
-	echo "Erreur, merci de donner un nom de fichier (./$(basename $0) -h)" >&2
+	echo "Error, enter a file name (./$(basename $0) -h)." >&2
 	exit 1
 fi
 
@@ -340,7 +326,7 @@ while [ $# -ne 0 ]; do
 	case $1 in
 		"-html")
 			if [ -e "$fic.html" ]; then
-				echo "Le fichier \"$fic.html\" existe déjà dans le répertoire courant"
+				echo "The file \"$fic.html\" already exists in this directory."
 				exit 1
 			else
 				if [ $languageCpt -eq 0 ]; then
@@ -348,7 +334,7 @@ while [ $# -ne 0 ]; do
 					languageCpt=1
 					shift
 				else
-					echo "Erreur, plusieurs formats de fichier saisit (./$(basename $0) -h)" >&2
+					echo "Error, multiple file formats entered (./$(basename $0) -h)." >&2
 					exit 1
 				fi
 			fi
@@ -356,7 +342,7 @@ while [ $# -ne 0 ]; do
 
 		"-py")
 			if [ -e "$fic.py" ]; then
-				echo "Le fichier \"$fic.py\" existe déjà dans le répertoire courant"
+				echo "The file \"$fic.py\" already exists in this directory."
 				exit 1
 			else
 				if [ $languageCpt -eq 0 ]; then
@@ -364,7 +350,7 @@ while [ $# -ne 0 ]; do
 					languageCpt=1
 					shift
 				else
-					echo "Erreur, plusieurs formats de fichier saisit (./$(basename $0) -h)" >&2
+					echo "Error, multiple file formats entered (./$(basename $0) -h)." >&2
 					exit 1
 				fi
 			fi
@@ -372,7 +358,7 @@ while [ $# -ne 0 ]; do
 		
 		"-sh")
 			if [ -e "$fic.sh" ]; then
-				echo "Le fichier \"$fic.sh\" existe déjà dans le répertoire courant"
+				echo "The file \"$fic.sh\" already exists in this directory."
 				exit 1
 			else
 				if [ $languageCpt -eq 0 ]; then
@@ -380,7 +366,7 @@ while [ $# -ne 0 ]; do
 					languageCpt=1
 					shift
 				else
-					echo "Erreur, plusieurs formats de fichier saisit (./$(basename $0) -h)" >&2
+					echo "Error, multiple file formats entered (./$(basename $0) -h)." >&2
 					exit 1
 				fi
 			fi
@@ -388,7 +374,7 @@ while [ $# -ne 0 ]; do
 
 		"-c")
 			if [ -e "$fic.c" ] ; then
-				echo "Le fichier \"$fic.c\" existe déjà dans le répertoire courant" >&2
+				echo "The file \"$fic.c\" already exists in this directory." >&2
 				exit 1
 			else
 				if [ $languageCpt -eq 0 ]; then
@@ -405,7 +391,7 @@ while [ $# -ne 0 ]; do
 						fi
 					done
 				else
-					echo "Erreur, plusieurs formats de fichier saisit (./$(basename $0) -h)" >&2
+					echo "Error, multiple file formats entered (./$(basename $0) -h)." >&2
 					exit 1
 				fi
 			fi
@@ -427,14 +413,14 @@ while [ $# -ne 0 ]; do
 			;;
 
 		*)
-			echo "Veuillez saisir une option valide (./$(basename $0) -h)" >&2
+			echo "Error: enter e valid option (./$(basename $0) -h)." >&2
 			exit 1
 			;;
 	esac
 done
 
 if [ -e $fic -a $languageCpt -eq 0 ]; then
-	echo "Le fichier ou le répertoire \"$fic\" existe déjà dans le répertoire courant"
+	echo "The file of the directory \"$fic\" already exists in this directory."
 else
 	creation
 fi
